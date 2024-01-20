f5c04ef4a75dcaac882f4fc35ceaf901559f7ef2
7up11 <83753733+7up11@users.noreply.github.com>
2024-01-20 16:11:52 -0500

    Add alert details template

--- dashboard/server.py
+++ dashboard/server.py
@@ -5,5 +5,10 @@ app = Flask(__name__)
 
 
 @app.route("/")
-def index():
+def index_page():
     return render_template("index.html")
+
+
+@app.route("/alert/<int:id>")
+def alert_page(id):
+    return render_template("alert.html")
